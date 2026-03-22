a8a85f42fdab8344776bb70b1446deb28978742d
yd8ccrrc88-coder <yd8ccrrc88@privaterelay.appleid.com>
2026-03-22 23:10:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,32 @@ function weightedRandom() {
     if (r <= 0) return p;
   }
 }
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = document.querySelector("img").clientWidth;
+  canvas.height = document.querySelector("img").clientHeight;
+}
+window.onload = resizeCanvas;
+window.onresize = resizeCanvas;
+
+let drawing = false;
+
+canvas.addEventListener("pointerdown", () => drawing = true);
+canvas.addEventListener("pointerup", () => drawing = false);
+canvas.addEventListener("pointermove", draw);
+
+function draw(e) {
+  if (!drawing) return;
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
